065ba55ef56873dad049b1841f792f70aa1a6583
Angelina <chulakovaav@yandex.ru>
2022-10-13 16:14:00 +0300

    add answers

---
+++ task_64/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+string OrderOfPositiveNumbers(int n)
+{
+    if (n > 0) return $"{n} " + OrderOfPositiveNumbers(n - 1);
+    return String.Empty;
+}
+
+string OrderOfNegativeNumbers(int n)
+{
+    if (n < 1) return $"{n} " + OrderOfNegativeNumbers(n + 1);
+    return String.Empty;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+{
+    Console.WriteLine($"N = {n} - > {OrderOfPositiveNumbers(n)}");
+}
+else if (n < 0)
+{
+    Console.WriteLine($"N = {n} - > {OrderOfNegativeNumbers(n)}");
+}
+else
+    Console.WriteLine($"N = {n} - > 0 1");
+
---
+++ task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumberMaxN(int m, int n)
+{
+    if (m == n) return m;
+    return m + SumNumberMaxN(m+1, n);
+}
+
+int SumNumberMaxM(int n, int m)
+{
+    if (n == m) return n;
+    return n + SumNumberMaxM(n+1, m);
+}
+
+Console.Write("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ( m < n)
+{
+ int x = SumNumberMaxN(m,n);
+Console.WriteLine($"Сумма чисел от {m} до {n} = {x}");
+}
+else if(n < m)
+{
+    int y = SumNumberMaxM(n,m);
+    Console.WriteLine($"Сумма чисел от {n} до {m} = {y}");
+}
+else 
+{
+    Console.WriteLine("Эти числа равны. Попробуйте ввести другие числа");
+}
